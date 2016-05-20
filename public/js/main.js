46c2596053a65b158360b4c045340c5c9a06c28a
Seth Battin <seth.battin@gmail.com>
2016-05-19 23:45:30 -0500

    finish island data

--- public/js/main.js
+++ public/js/main.js
@@ -135,10 +135,10 @@
                 context.font = "bolder 32px arial";
                 if (input.isUIOver('setsail')){
                     context.fillStyle = "white";
-                    context.fillText('Set Sail', 91, 51);
+                    context.fillText('Set Sail', 91, 71);
                 }
                 context.fillStyle = "black";
-                context.fillText('Set Sail', 90, 50);
+                context.fillText('Set Sail', 90, 70);
                 
                 context.restore();
             }
@@ -243,38 +243,73 @@
             context.fillText(activeIsland.name, c.x, c.y);
             context.restore();
             activeIsland.drawUI(context);
-        };
+        };  
         
         
         return this;
     })();
     
-    worldMap.addIsland('island1', -215, 280, 65, 70);
-    worldMap.addIsland('island2', -30, 265, 75, 50);
-    worldMap.addIsland('island3', 180, 290, 70, 55);
-    worldMap.addIsland('island4', -395, 240, 85, 70);
-    worldMap.addIsland('island5', -250, 180, 75, 60);
-    worldMap.addIsland('island6', -115, 165, 75, 60);
-    worldMap.addIsland('island7', 5, 170, 150, 75);
-    worldMap.addIsland('tree isle', 195, 165, 80, 95);
-    worldMap.addIsland('runt island', 320, 240, 55, 45);
+    var islandData = [
     
+        ['island1', -215, 280, 65, 70, ['island4', 'island5', 'island6']],
+        ['island2', -30, 265, 75, 50, ['island6', 'island7']],
+        ['island3', 180, 290, 70, 55, ['island7', 'tree isle', 'runt island']],
+        ['island4', -395, 240, 85, 70, ['sperm diamond island', 'jacked hylian']],
+        ['island5', -250, 180, 75, 60, ['jacked hylian', 'stark banner']],
+        ['island6', -115, 165, 75, 60, ['jacked hylian', 'stark banner']],
+        ['island7', 5, 170, 150, 75, ['stark banner', 'V for vagina']],
+        ['tree isle', 195, 165, 80, 95, ['V for vagina', 'spear island']],
+        ['runt island', 320, 240, 55, 45, ['spear island']],
+        ['sperm diamond island', -450, 85, 100, 90, ['danger penis']],
+        ['jacked hylian', -305, 65, 85, 75, ['danger penis', 'crater bowl']],
+        ['stark banner', -115, 50, 130, 60, ['crater bowl', 'i like turtles']],
+        ['V for vagina', 100, 55, 80, 70, ['i like turtles', 'egg of doom']],
+        ['spear island', 215, 95, 200, 85, ['left mount', 'Chondrichthyes']],
+        ['danger penis', -480, -55, 255, 80, ['trapped pacman', 'triforce']],
+        ['crater bowl', -165, -55, 65, 70, ['horny island', 'Old God\'s Island \\ Pushishment Isle (disputed)']],
+        ['i like turtles', -20, -50, 80, 70, ['Old God\'s Island \\ Pushishment Isle (disputed)', 'Zen Isle']],
+        ['egg of doom', 135, -50, 70, 85, ['Zen Isle', 'crappy rocks']],
+        ['left mouth', 280, -10, 80, 50, ['crappy rocks', 'dagger island']],
+        ['Chondrichthyes', 380, -5, 120, 75, ['crappy rocks', 'dagger island']],
+        ['trapped pacman', -480, -160, 100, 95, ['mega plug']],
+        ['triforce', -340, -170, 90, 75, ['mega plug']],
+        ['horny island', -200, -165, 80, 80, ['mega plug 2']],
+        ['Old God\'s Island \\ Pushishment Isle (disputed)', 
+                                            -75, -165, 90, 90, ['mega plug 2']],
+        ['Zen Isle', 95, -170, 90, 80, ['round land']],
+        ['crappy rocks', 290, -115, 70, 50, ['round land', 'the bat, man']],
+        ['dagger island', 415, -105, 75, 75, ['the bat, man']],
+        ['mega plug', -455, -240, 160, 75, ['round land 2']],
+        ['mega plug 2', -180, -240, 180, 65, ['round land 3']],
+        ['round land', 175, -225, 65, 50, ['death lizard']],
+        ['the bat, man', 325, -230, 165, 75, ['death lizard']],
+        ['round land 2', -320, -325, 85, 75, ['the end']],
+        ['round land 3', -115, -320, 90, 60, ['the end']],
+        ['death lizard', 110, -350, 110, 90, ['the end']],
+        ['the end', -100, -380, 80, 80, []]
+    ];
+    
+    for( var i = 0; i < islandData.length; ++i){
+        worldMap.addIsland(
+            islandData[i][0],
+            islandData[i][1],
+            islandData[i][2],
+            islandData[i][3],
+            islandData[i][4]
+        );
+    }
+
     var mapRoot = worldMap.initGraph(
         new worldMap.Island('start', 0, 350, 1, 1));
     worldMap.addIslandPath('start', 'island1');
     worldMap.addIslandPath('start', 'island2');
     worldMap.addIslandPath('start', 'island3');
-    
-    worldMap.addIslandPath('island1', 'island4');
-    worldMap.addIslandPath('island1', 'island5');
-    worldMap.addIslandPath('island1', 'island6');
-    
-    worldMap.addIslandPath('island2', 'island6');
-    worldMap.addIslandPath('island2', 'island7');
-    
-    worldMap.addIslandPath('island3', 'island7');
-    worldMap.addIslandPath('island3', 'tree isle');
-    worldMap.addIslandPath('island3', 'runt island');
+
+    for( var i = 0; i < islandData.length; ++i){
+        for (var j = 0; j < islandData[i][5].length; ++j){
+            worldMap.addIslandPath(islandData[i][0], islandData[i][5][j]);
+        }
+    }
 
     loop.addItem(worldMap);
 
